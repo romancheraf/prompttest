d61aef0f6297cb2b96a04bcdfac38d3b6848fe05
romancheraf <139028718+romancheraf@users.noreply.github.com>
2023-07-09 14:31:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: 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,
-});
-
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+const API_KEY = "YOUR_OPENAI_API_KEY";
 
 var generateButton = document.getElementById('generate-button');
-var userInput = document.getElementById('user-input');
+var mealTypeInput = document.getElementById('meal-type');
+var targetCaloriesInput = document.getElementById('target-calories');
+var dietTypeInput = document.getElementById('diet-type');
+var prepTimeInput = document.getElementById('prep-time');
 var outputContainer = document.getElementById('output-container');
 
 generateButton.addEventListener('click', function() {
-  var thoughts = userInput.value;
+    var mealType = mealTypeInput.value;
+    var targetCalories = targetCaloriesInput.value;
+    var dietType = dietTypeInput.value;
+    var prepTime = prepTimeInput.value;
 
-  var requestData = {
-    model: 'text-davinci-003',
-    prompt: thoughts,
-    max_tokens: 50 
-  };
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + API_KEY
+    };
 
-  openai.complete(requestData)
+    var prompt = "I want a [" + mealType + "] meal. My target Calories are [" + targetCalories + " cal] from this meal. My type of diet is [" + dietType + "]. Time to prep is [" + prepTime + " minutes].";
+
+    var requestData = {
+        model: 'text-davinci-003',
+        prompt: prompt,
+        max_tokens: 100 
+    };
+
+    fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(requestData)
+    })
     .then(function(response) {
-      var generatedPrompts = response.choices.map(function(choice) {
-        return choice.text.trim();
-      });
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Network response was not OK');
+        }
+    })
+    .then(function(data) {
+        var mealPrepIdea = data.choices[0].text.trim();
 
-      outputContainer.innerHTML = generatedPrompts.join('<br>');
+        outputContainer.innerHTML = '<h2>Generated Meal Prep Idea:</h2><p>' + mealPrepIdea + '</p>';
     })
     .catch(function(error) {
-      console.error('Error:', error);
-      outputContainer.innerHTML = 'An error occurred. Please try again.';
+        console.error('Error:', error);
+        outputContainer.innerHTML = 'An error occurred. Please try again.';
     });
 });
