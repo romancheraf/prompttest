76483b035c6649810bdf3ca5156816bb46581c8e
romancheraf <139028718+romancheraf@users.noreply.github.com>
2023-07-09 13:43:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+var generateButton = document.getElementById('generate-button');
+var userInput = document.getElementById('user-input');
+var outputContainer = document.getElementById('output-container');
+
+generateButton.addEventListener('click', function() {
+    var thoughts = userInput.value;
+
+    var apiKey = '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'; 
+
+    var headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + apiKey
+    };
+
+    var requestData = {
+        prompt: thoughts,
+        max_tokens: 100 
+    };
+
+    fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(requestData)
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var generatedPrompts = data.choices.map(function(choice) {
+            return choice.text.trim();
+        });
+
+        outputContainer.innerHTML = generatedPrompts.join('<br>');
+    })
+    .catch(function(error) {
+        console.error('Error:', error);
+        outputContainer.innerHTML = 'An error occurred. Please try again.';
+    });
+});
