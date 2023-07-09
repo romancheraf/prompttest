838d2899f4f2ac9329fff249e096e424d162c22e
romancheraf <139028718+romancheraf@users.noreply.github.com>
2023-07-09 14:25:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: 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,
+});
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.listEngines();
+
 var generateButton = document.getElementById('generate-button');
 var userInput = document.getElementById('user-input');
 var outputContainer = document.getElementById('output-container');
 
 generateButton.addEventListener('click', function() {
-    var thoughts = userInput.value;
+  var thoughts = userInput.value;
 
-    var apiKey = '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'; 
+  var requestData = {
+    model: 'text-davinci-003',
+    prompt: thoughts,
+    max_tokens: 50 
+  };
 
-    var headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + apiKey
-    };
-
-    var requestData = {
-        model: 'text-davinci-003',
-        prompt: thoughts,
-        max_tokens: 50 
-    };
-
-    fetch('https://api.openai.com/v1/completions', {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(requestData)
-    })
+  openai.complete(requestData)
     .then(function(response) {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error('Network response was not OK');
-        }
-    })
-    .then(function(data) {
-        var generatedPrompts = data.choices.map(function(choice) {
-            return choice.text.trim();
-        });
+      var generatedPrompts = response.choices.map(function(choice) {
+        return choice.text.trim();
+      });
 
-        outputContainer.innerHTML = generatedPrompts.join('<br>');
+      outputContainer.innerHTML = generatedPrompts.join('<br>');
     })
     .catch(function(error) {
-        console.error('Error:', error);
-        outputContainer.innerHTML = 'An error occurred. Please try again.';
+      console.error('Error:', error);
+      outputContainer.innerHTML = 'An error occurred. Please try again.';
     });
 });
